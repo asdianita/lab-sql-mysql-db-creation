8ca8f6b1468b6ae2b5354db79eaeae7ac90ef01e
asdianita <77894735+asdianita@users.noreply.github.com>
2024-11-15 21:04:58 +0100

    revision comentarios

--- create.sql
+++ create.sql
@@ -2,44 +2,47 @@ CREATE DATABASE IF NOT EXISTS lab_mysql;
 USE lab_mysql;
 
 DROP TABLE IF EXISTS cars;
+
 CREATE TABLE cars (
-id int AUTO_INCREMENT PRIMARY KEY,
-vin varchar(25),
-manufacturer varchar(25),
-model varchar(25),
-car_year int(4),
-color varchar(25)
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    vin VARCHAR(17) NOT NULL UNIQUE,
+    manufacturer VARCHAR(50) NOT NULL,
+    model VARCHAR(50) NOT NULL,
+    car_year YEAR NOT NULL,
+    color VARCHAR(20) NOT NULL
 );
 
 DROP TABLE IF EXISTS customers;
 CREATE TABLE customers (
-id int AUTO_INCREMENT PRIMARY KEY, 
-cust_id int,
-cust_name varchar(25),
-cust_phone varchar(25),
-cust_email varchar(25),
-cust_address varchar(25),
-cust_city varchar(25),
-cust_state varchar(25),
-cust_country varchar(25),
-cust_zipcode int(5)
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    cust_id INT UNSIGNED NOT NULL,
+    cust_name VARCHAR(50) NOT NULL,
+    cust_phone VARCHAR(20),
+    cust_email VARCHAR(50),
+    cust_address VARCHAR(100) NOT NULL,
+    cust_zipcode INT UNSIGNED NOT NULL,
+    CONSTRAINT unique_cust_id UNIQUE (cust_id)
 );
+
 
 DROP TABLE IF EXISTS salespersons;
 CREATE TABLE salespersons (
-id int AUTO_INCREMENT PRIMARY KEY, 
-staff_id int,
-staff_name varchar(25),
-store varchar(25)
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    staff_id VARCHAR(10) NOT NULL UNIQUE,
+    name VARCHAR(50) NOT NULL,
+    store VARCHAR(50) NOT NULL
 );
 
 DROP TABLE IF EXISTS invoices;
 CREATE TABLE invoices (
-id int AUTO_INCREMENT PRIMARY KEY, 
-invoice_number int(10),
-invoice_date date,
-car int,
-customer int,
-salesperson int
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    invoice_number INT UNSIGNED NOT NULL UNIQUE,
+    date DATE NOT NULL,
+    car_id INT UNSIGNED NOT NULL,
+    customer_id INT UNSIGNED NOT NULL,
+    salesperson_id INT UNSIGNED NOT NULL,
+    FOREIGN KEY (car_id) REFERENCES cars(id),
+    FOREIGN KEY (customer_id) REFERENCES customers(id),
+    FOREIGN KEY (salesperson_id) REFERENCES salespersons(id)
 );
 
--- update.sql
+++ update.sql
@@ -1,11 +1,10 @@
 UPDATE customers
-SET email = 'ppicasso@gmail.com'
-WHERE name = 'Pablo Picasso';
+SET cust_email = 'ppicasso@gmail.com' WHERE cust_id = 10001;
 
 UPDATE customers
 SET email = 'lincoln@us.gov'
-WHERE name = 'Abraham Lincoln';
+WHERE cust_id = 20001;
 
 UPDATE customers
 SET email = 'hello@napoleon.me'
-WHERE name = 'Napoléon Bonaparte';
+WHERE cust_id = 30001;
